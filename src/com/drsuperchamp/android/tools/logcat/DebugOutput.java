092ee6e47160297d684338bcd8afe6463e1fcbc9
dr.superchamp <dr.superchamp@gmail.com>
2011-09-29 09:18:50 +0000

    added ant build script and copyright statement.

--- src/com/drsuperchamp/android/tools/logcat/DebugOutput.java
+++
@@ -1,5 +0,0 @@
-package com.drsuperchamp.android.tools.logcat;
-
-public interface DebugOutput {
-    void out(String message);
-}
--- src/com/drsuperchamp/android/tools/logcat/FilterOutput.java
+++
@@ -1,7 +0,0 @@
-package com.drsuperchamp.android.tools.logcat;
-
-import com.drsuperchamp.android.tools.logcat.LogCatWrapper.LogMessage;
-
-public interface FilterOutput {
-    void out(String name, LogMessage []newMessages, int numRemoved);
-}
