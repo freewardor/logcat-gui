e8258e7fa1908c346cf4deb7f4fe58d885ec78a4
dr.superchamp <dr.superchamp@gmail.com>
2011-11-22 09:57:09 +0000

    initial ui implementation.. not finished...-_-

--- src/com/drsuperchamp/android/tools/logcat/ui/MainFrame.java
+++ src/com/drsuperchamp/android/tools/logcat/ui/MainFrame.java
@@ -1,19 +1,26 @@
 package com.drsuperchamp.android.tools.logcat.ui;
 
 import com.drsuperchamp.android.tools.logcat.core.AdbWrapper;
+import com.drsuperchamp.android.tools.logcat.core.LogCatWrapper;
 import com.drsuperchamp.android.tools.logcat.core.AdbWrapper.DeviceConnectionListener;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JToolBar;
+import javax.swing.JViewport;
 import javax.swing.SwingUtilities;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JTabbedPane;
@@ -24,15 +31,20 @@ import java.awt.event.ActionEvent;
 
 
 public class MainFrame extends JFrame implements DeviceConnectionListener {
+    private static final String DEFAULT_TABLE_NAME = "Log";
     private final String ADB_BIN_PATH;
     private AdbWrapper mAdb = null;
     private String mConnectedDevSerialNum = null;
-    private List<JTable> mTables = new ArrayList<JTable>();
-    private LogTableModel mLogModel;
-    private JButton mBtnConnect;
+    private LogCatWrapper mLogcat;
+    private JTabbedPane mTabbedPane = null;
+    private JButton mBtnConnect = null;
+    private JButton mBtnAddFilter = null;
+    private JButton mBtnRemoveFilter = null;
+    private List<LogTable> mTables = new ArrayList<LogTable>();
 
     public MainFrame(String adb_bin_path) {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 640, 480);
 
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
@@ -49,33 +61,99 @@ public class MainFrame extends JFrame implements DeviceConnectionListener {
         mBtnConnect = new JButton("No device");
         mBtnConnect.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
-                LogTableUpdater updater = new LogTableUpdater(mLogModel);
+                mBtnConnect.setEnabled(false);
+                mBtnAddFilter.setEnabled(true);
+                mBtnRemoveFilter.setEnabled(true);
+
+                removeAllTables(mTabbedPane);
+
+                LogTable defaultTable = findTable(DEFAULT_TABLE_NAME);
+                defaultTable.clear();
+                addTable(mTabbedPane, defaultTable);
+
+                mLogcat = new LogCatWrapper(mConnectedDevSerialNum, 10000);
+                mLogcat.setDefaultFilterOutput(defaultTable);
+
+                Iterator<LogTable> it = mTables.iterator();
+                while(it.hasNext()) {
+                    LogTable table = it.next();
+                    if (table.getName().equals(DEFAULT_TABLE_NAME)) {
+                        continue;
+                    }
+                    table.clear();
+                    addTable(mTabbedPane, table);
+                    mLogcat.addFilter(table.getName(), table.filterTag(),
+                            table.filterPid(), table.filterLogLevel(), null, table);
+                }
+
+                // added filters
+                new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+                        mAdb.executeShellCommand(mConnectedDevSerialNum, "logcat -v long", mLogcat.getShellOutputReceiver());
+                    }
+                }).start();
             }
         });
         mBtnConnect.setEnabled(false);
         toolBar.add(mBtnConnect);
 
-        JButton btnAddAFilter = new JButton("Add a filter");
-        toolBar.add(btnAddAFilter);
+        mBtnAddFilter = new JButton("Add a filter");
+        mBtnAddFilter.setEnabled(false);
+        toolBar.add(mBtnAddFilter);
 
-        JButton btnRemoveAFilter = new JButton("Remove a filter");
-        toolBar.add(btnRemoveAFilter);
+        mBtnRemoveFilter = new JButton("Remove a filter");
+        mBtnRemoveFilter.setEnabled(false);
+        toolBar.add(mBtnRemoveFilter);
 
-        JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+        mTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        getContentPane().add(mTabbedPane, BorderLayout.CENTER);
 
-        JScrollPane scrollPane = new JScrollPane();
-        tabbedPane.addTab("Log", null, scrollPane, null);
+        LogTable defaultTable = new LogTable(DEFAULT_TABLE_NAME);
+        mTables.add(defaultTable);
 
-        mLogModel = new LogTableModel();
-        JTable table = new JTable(mLogModel);
-        table.setName("log_table");
-        scrollPane.setViewportView(table);
-        mTables.add(table);
+        LogTable micomTable = new LogTable("Micom");
+        micomTable.setFilterValue("micom", null, "V");
+        mTables.add(micomTable);
 
         ADB_BIN_PATH = adb_bin_path;
         mAdb = AdbWrapper.getInstance();
         mAdb.connect(ADB_BIN_PATH, this);
+    }
+
+    private LogTable findTable(String name) {
+        Iterator<LogTable> it = mTables.iterator();
+        while(it.hasNext()) {
+            LogTable table = it.next();
+            if (table.getName().equals(name)) {
+                return table;
+            }
+        }
+        return null;
+    }
+
+    private void addTable(JTabbedPane tabbedPane, LogTable table) {
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportView(table);
+        tabbedPane.addTab(table.getName(), null, scrollPane, table.getName());
+    }
+
+    private void removeTable(JTabbedPane tabbedPane, LogTable table) {
+        Component target = table.getParent();
+        int loop_len = tabbedPane.getTabCount();
+        for(int n=0; n < loop_len; n++) {
+            Component component = tabbedPane.getTabComponentAt(n);
+            if (component == target) {
+                tabbedPane.removeTabAt(n);
+                return;
+            }
+        }
+    }
+
+    private void removeAllTables(JTabbedPane tabbedPane) {
+        while(tabbedPane.getTabCount() > 0) {
+            tabbedPane.removeTabAt(0);
+        }
     }
 
     @Override
@@ -110,6 +188,8 @@ public class MainFrame extends JFrame implements DeviceConnectionListener {
             public void run() {
                 mBtnConnect.setText("No device");
                 mBtnConnect.setEnabled(false);
+                mBtnAddFilter.setEnabled(false);
+                mBtnRemoveFilter.setEnabled(false);
             }
         }, true);
     }
